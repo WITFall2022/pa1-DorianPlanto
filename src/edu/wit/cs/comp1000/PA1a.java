21f489fd1f2af5f4a9a0edb12861ea9dabc16e05
DorianPlanto <114677459+DorianPlanto@users.noreply.github.com>
2022-11-03 11:07:38 -0400

    i complete all the assignement

--- src/edu/wit/cs/comp1000/PA1a.java
+++ src/edu/wit/cs/comp1000/PA1a.java
@@ -1,11 +1,32 @@
 package edu.wit.cs.comp1000;
-
+import java.util.Scanner;
 // TODO: document this class
 public class PA1a {
 
 	// TODO: document this function
 	public static void main(String[] args) {
-		// TODO: write your code here
+ Scanner input = new Scanner(System.in);
+		
+        int yards;
+        int feet;
+        int inches;
+        
+        System.out.printf("Enter number of yards");
+        yards = input.nextInt();
+        
+        System.out.printf("Enter number of feet");
+        feet = input.nextInt();
+        
+        System.out.printf("Enter number of inches");
+        inches = input.nextInt();
+        
+        int TotalInches = (yards * 36) + (feet * 12) + inches;
+        
+        System.out.printf("Total numer of inches:%d", TotalInches);
+	}
+
+	 {
+	
 	}
 
 }
